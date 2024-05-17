b212346b30deb09bcbb661a1f7d841e5f575bca3
Lucas Nóbrega Rodrigues <lucasnrdrigues@gmail.com>
2024-05-16 23:42:50 -0300

    Copiando a pasta 12 do prof

--- 12-api-empresas/src/index.js
+++ 12-api-empresas/src/index.js
@@ -1,19 +1,16 @@
-//Configs iniciais
 const express = require('express')
 const app = express()
 const PORT = 3000
 
-//Fazendo a conexão
-const DBConnect = require('./database/conection')
+const DBConnect = require('./database/connection')
 DBConnect()
 
-//Configurando o middleware
 app.use(express.json())
 
 const routes = require('./routes/routes')
+app.use(routes)
 
 
-//Configurando a porta de "escuta"
 app.listen(PORT, () => {
     console.log(`Aplicação rodando na porta ${PORT}`)
 })
--- 12-api-empresas/src/routes/routes.js
+++ 12-api-empresas/src/routes/routes.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const router = express.Router()
 
-const CargoControler = require('../controllers/CargoController.js')
+// controllers
+const CargoController = require('../controllers/CargoController')
 
-//cargo
-router.post('/cargos', CargoControler.criar) //nosso controle já está pronto, ent não precisa usar o "(req, res)" aqui
+// validators
+const { validarCargo } = require('../validators/CargoValidator')
+
+
+// Cargo
+router.get('/cargos', CargoController.buscarTodos)
+router.get('/cargos/:id', CargoController.buscarPorID)
+router.post('/cargos', validarCargo, CargoController.criar)
+router.put('/cargos/:id', validarCargo, CargoController.atualizar)
+router.delete('/cargos/:id', CargoController.excluir)
+
+
+// Funcionarios
+
+
+
+// Departamentos
 
 
 
